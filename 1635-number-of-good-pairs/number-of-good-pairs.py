49031e12309191c5720cd7815704e971e7cdfdc6
varunmj123 <61578176+varunmj123@users.noreply.github.com>
2024-08-15 13:53:19 -0600

    Time: 36 ms (62.46%) | Memory: 16.5 MB (63.39%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        my_count = 0
+        my_dict = {}
+        
+        for n in nums:
+            # Check to see if number has already been encountered
+            if n in my_dict:
+                # Increase count by the number of previous instances
+                my_count += my_dict[n]
+
+                # Increase the count of previous observation
+                my_dict[n] +=1
+            else:
+                # Store newly encountered number along with its count
+                my_dict[n] = 1
+                
+        return my_count
