3668beeefaf1d8f49ad0951fe9ad68469bc63c7c
varunmj123 <61578176+varunmj123@users.noreply.github.com>
2024-09-02 13:27:48 -0600

    Time: 39 ms (71.25%) | Memory: 17.4 MB (83.06%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -1,19 +1,25 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
-    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        
-        def dfs(root, total):
-            if not root:
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        def backtrack(node, current_sum):
+            if not node:
                 return False
-            total += root.val
-            if not root.left and not root.right:
-                return total == targetSum
-            left = dfs(root.left, total)
-            right = dfs(root.right, total)
-            return left or right 
-        return dfs(root, 0)
+            current_sum += node.val
+
+            if not node.left and not node.right and current_sum == targetSum:
+                return True
+
+            left = backtrack(node.left, current_sum)
+            right = backtrack(node.right, current_sum)
+
+            current_sum -= node.val
+            
+            return left or right
+
+
+        return backtrack(root, 0)
