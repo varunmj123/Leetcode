d97b4ac3cbb448799ce3cff5686b90c77fe43e14
varunmj123 <61578176+varunmj123@users.noreply.github.com>
2024-09-08 23:57:29 -0600

    Time: 350 ms (86.52%) | Memory: 17 MB (59.85%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,24 +1,15 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        visited = set()
-        total = 0
-        def dfs(r,c):
-            if (r not in range(rows) or c not in range(cols) or grid[r][c] == 0):
-                return 0
-            if grid[r][c] == 1:
-                return 1
-
+        rows, cols = len(grid), len(grid[0])
+        perimeter = 0
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
-                    covered = 0
-                    for dr, dc in directions:
-                        covered += dfs(r + dr,c + dc)
-                    total += (4 - covered)
-        return total
-
-                    
-        
+                    # Check each cell's four potential sides
+                    perimeter += 4
+                    if r > 0 and grid[r-1][c] == 1:
+                        perimeter -= 2
+                    if c > 0 and grid[r][c-1] == 1:
+                        perimeter -= 2
+        return perimeter
